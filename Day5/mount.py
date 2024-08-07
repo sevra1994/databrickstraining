b6946d03ad8907a1aebadd89c399e7024146675a
sevra1994 <sevra1994>
2024-08-07 04:31:40 +0000

    mount

---
+++ Day5/mount.py
@@ -0,0 +1,7 @@
+# Databricks notebook source
+""" Azure Path
+dbutils.fs.mount(
+  source = {wasbs://{Path}.blob.core.windows.net},
+  mount_point = {Folder Location},
+  extra_configs = {Key Path:"{Access Kay}"})
+  """
